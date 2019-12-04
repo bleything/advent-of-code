e8f5fef2f5dee85a2cfc5f22a1425d4f49ad6928
Ben Bleything <bleything@google.com>
2019-12-04 14:18:09 -0800

    add 2019/01 part two

--- 2019/01/solution.rb
+++ 2019/01/solution.rb
@@ -6,16 +6,33 @@ class Solution
     input / 3 - 2
   end
 
+  def solve_part_two(input)
+    result = solve(input)
+
+    if result < 1
+      return 0
+    else
+      return result + solve_part_two(result)
+    end
+
+    return result
+  end
+
 end
 
 if __FILE__ == $0
   s = Solution.new
-  sum = 0
+  part1 = 0
+  part2 = 0
 
   File.readlines("input.txt").each do |line|
-    sum += s.solve(line.to_i)
+    l = line.to_i
+
+    part1 += s.solve(l)
+    part2 += s.solve_part_two(l)
   end
 
-  puts sum
+  puts "Part One: #{part1}"
+  puts "Part Two: #{part2}"
 
 end

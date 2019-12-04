7a7511146085f800ab006b85c73409cc8c2b3f18
Ben Bleything <bleything@google.com>
2019-12-04 13:21:43 -0800

    add solution template

---
+++ .template/solution.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+class Solution
+
+  def solve(*args)
+  end
+
+end
+
+if __FILE__ == $0
+  s = Solution.new
+  # read input, format results, etc
+end
---
+++ .template/test.rb
@@ -0,0 +1,13 @@
+require 'test-unit'
+require './solution'
+
+class TestSolution < Test::Unit::TestCase
+
+  def setup
+    @solution = Solution.new
+  end
+
+  def test_examples
+    # write assertions for the given examples
+  end
+end

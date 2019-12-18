9ee3163cd20abc95ac82f61edde40eb98f1c7ba5
Ben Bleything <bleything@google.com>
2019-12-17 22:38:41 -0800

    minor refactoring/tidying

--- 2019/lib/intcode.rb
+++ 2019/lib/intcode.rb
@@ -1,16 +1,16 @@
 class Intcode
   attr_reader :state
 
-  OPCODES = {
-    1  => Proc.new {|a,b| a + b},
-    2  => Proc.new {|a,b| a * b},
-    99 => Proc.new { @running = false }
-  }
-
-  def initialize(program)
+  def initialize(program, debug = false)
     @state = program
     @ptr = 0
     @running = true
+
+    @debug = debug
+  end
+
+  def debug(msg)
+    puts "DEBUG: #{msg}" if @debug
   end
 
   def run!
@@ -22,28 +22,25 @@ class Intcode
   end
 
   def step(opcode)
-    if opcode == 99
-      @running = false
-      return
-    end
+    operations = {
+      1  => lambda {|a, b, dest| @state[dest] = @state[a] + @state[b] },
+      2  => lambda {|a, b, dest| @state[dest] = @state[a] * @state[b] },
+      99 => lambda { @running = false }
+    }
 
-    a_addr = @state[@ptr + 1]
-    b_addr = @state[@ptr + 2]
-    r_addr = @state[@ptr + 3]
+    op = operations[opcode]
 
-    # p @state
-    # puts "p%02i o%02i a%02i b%02i"  % [@ptr, opcode, a_addr, b_addr]
+    params = []
+    op.arity.times {|idx| params << @state[@ptr + idx + 1] }
 
-    a = @state[a_addr]
-    b = @state[b_addr]
+    debug "opcode #{opcode}"
+    debug "params #{params}" if params.any?
 
-    case opcode
-    when 1
-           @state[r_addr] = a + b
-    when 2
-           @state[r_addr] = a * b
-    end
+    op.call *params
 
-    @ptr += 4
+    # advance the program counter by 1 (for the opcode) plus however many params
+    # there were
+    @ptr += (1 + op.arity)
   end
+
 end

03f242cf1211253e8ebd89c09ab1790878cbef93
Ben Bleything <bleything@google.com>
2019-12-17 20:09:06 -0800

    update 2019/01 to new template

--- 2019/01/solution.rb
+++ 2019/01/solution.rb
@@ -1,18 +1,32 @@
 #!/usr/bin/env ruby
 
 class Solution
+  attr_reader :part1, :part2
 
-  def solve(input)
+  def initialize(input = "")
+    @lines = input.lines.map(&:strip).map(&:to_i)
+    @part1 = 0
+    @part2 = 0
+  end
+
+  def solve!
+    @lines.each do |value|
+      @part1 += part_one value
+      @part2 += part_two value
+    end
+  end
+
+  def part_one(input)
     input / 3 - 2
   end
 
-  def solve_part_two(input)
-    result = solve(input)
+  def part_two(input)
+    result = part_one(input)
 
     if result < 1
       return 0
     else
-      return result + solve_part_two(result)
+      return result + part_two(result)
     end
 
     return result
@@ -21,18 +35,10 @@ class Solution
 end
 
 if __FILE__ == $0
-  s = Solution.new
-  part1 = 0
-  part2 = 0
+  input = File.read("input.txt")
+  s = Solution.new(input)
+  s.solve!
 
-  File.readlines("input.txt").each do |line|
-    l = line.to_i
-
-    part1 += s.solve(l)
-    part2 += s.solve_part_two(l)
-  end
-
-  puts "Part One: #{part1}"
-  puts "Part Two: #{part2}"
-
+  puts "Part One: #{s.part1}"
+  puts "Part Two: #{s.part2}"
 end
--- 2019/01/test.rb
+++ 2019/01/test.rb
@@ -8,15 +8,15 @@ class TestSolution < Test::Unit::TestCase
   end
 
   def test_part_one_examples
-    assert_equal 2, @solution.solve(12)
-    assert_equal 2, @solution.solve(14)
-    assert_equal 654, @solution.solve(1969)
-    assert_equal 33583, @solution.solve(100756)
+    assert_equal 2, @solution.part_one(12)
+    assert_equal 2, @solution.part_one(14)
+    assert_equal 654, @solution.part_one(1969)
+    assert_equal 33583, @solution.part_one(100756)
   end
 
   def test_part_two_examples
-    assert_equal 2, @solution.solve_part_two(14)
-    assert_equal 966, @solution.solve_part_two(1969)
-    assert_equal 50346, @solution.solve_part_two(100756)
+    assert_equal 2, @solution.part_two(14)
+    assert_equal 966, @solution.part_two(1969)
+    assert_equal 50346, @solution.part_two(100756)
   end
 end

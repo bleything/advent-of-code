fb1b241bd52d2a815a6c1b6d5671cff9536f64fd
Ben Bleything <bleything@google.com>
2019-12-17 20:08:56 -0800

    update template

--- .template/solution.rb
+++ .template/solution.rb
@@ -1,13 +1,29 @@
 #!/usr/bin/env ruby
 
-class Solution
+# allows you to load files from <year>/lib. delete if you don't need it
+$: << File.expand_path(__FILE__ + "/../../lib")
 
-  def solve(*args)
+class Solution
+  attr_reader :part1, :part2
+
+  def initialize(*args)
+    # feel free to change the params if you want
+  end
+
+  def solve!
+    # do the meat of the solution here, assign results to @part1 and @part2
   end
 
 end
 
 if __FILE__ == $0
-  s = Solution.new
-  # read input, format results, etc
+  input = File.read("input.txt")
+
+  # do any munging you might need to do with the input
+
+  s = Solution.new(input)
+  s.solve!
+
+  puts "part one: #{s.part1}"
+  puts "part two: #{s.part2}"
 end
--- .template/test.rb
+++ .template/test.rb
@@ -5,9 +5,12 @@ class TestSolution < Test::Unit::TestCase
 
   def setup
     @solution = Solution.new
+    @solution.solve!
   end
 
   def test_examples
+    assert @solution.part1
+    assert @solution.part2
     # write assertions for the given examples
   end
 end
